1ce8bad72df40ed87cd76c82af0c072b3febec2d
Xcode <xcode@DSs-iMac.local>
2017-10-04 14:23:52 +0300

    finalize

--- MovementPlanViewController.swift
+++ MovementPlanViewController.swift
@@ -20,6 +20,9 @@ class MovementPlanViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //hide the keyboard on tap
+        self.hideKeyboardWhenTappedAround()
+        
         let nav = self.navigationController?.navigationBar
         nav?.barStyle = UIBarStyle.black
         nav?.tintColor = UIColor.white
@@ -54,6 +57,7 @@ class MovementPlanViewController: UIViewController {
     }
    
     @IBAction func saveClicked(_ sender: Any) {
+       if let prayer2 = prayerTextInput.text, let help2 = myhelpText.text, let win2 = winText.text, let build2 = buildText.text, let send2 = sendtext.text, (prayer2.characters.count > 0 && help2.characters.count > 0 && win2.characters.count > 0 && build2.characters.count > 0 && send2.characters.count > 0) {
         
         
         let myprayerTextInput = prayerTextInput.text
@@ -68,23 +72,37 @@ class MovementPlanViewController: UIViewController {
         UserDefaults.standard.set(mysendtext, forKey: "mySend")
         
         let AlertController = UIAlertController(title: "Your Movement Plan", message: "Your Movement Plan Saved Succesfully", preferredStyle: .alert)
-        let defaultAction = UIAlertAction(title: "Close Alert", style: .default, handler: nil)
+        let defaultAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
         AlertController.addAction(defaultAction)
         
         present(AlertController, animated: true, completion: nil)
+        }
         
+       else {
+        
+        let alert2 = UIAlertController(title: "Error", message: "Please, add your plans in all empty fields", preferredStyle: .alert)
+        alert2.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        present(alert2, animated: true, completion: nil)
+        
+        }
     }
 
     @IBAction func onShareClicked(_ sender: Any) {
         
-        
+        if let prayer1 = prayerTextInput.text, let help1 = myhelpText.text, let win1 = winText.text, let build1 = buildText.text, let send1 = sendtext.text, (prayer1.characters.count > 0 && help1.characters.count > 0 && win1.characters.count > 0 && build1.characters.count > 0 && send1.characters.count > 0) {
         
         let shareText = [" My Movement Plan \n\n Prayer \n Prayer Strategy: \n \(String(describing: prayerTextInput.text!)) \n\n My Help \n Who will hep: \n \(String(describing: myhelpText.text!)) \n\n Win \n Ways to reach students through relationships and evangelism: \n  \(String(describing: winText.text!)) \n\n Build \n How to create a ministry of discipleship: \n  \(String(describing: buildText.text!)) \n\n Send \n How students can lead locally and go globally: \n  \(String(describing: sendtext.text!))  " ]
         
         let activitycVC = UIActivityViewController(activityItems: shareText, applicationActivities: nil)
         activitycVC.popoverPresentationController?.sourceView = self.view
         self.present(activitycVC, animated: true, completion:  nil)
+        }
         
+        else {
+            let alert1 = UIAlertController(title: "Error", message: "Please, add your plans in all empty fields", preferredStyle: .alert)
+            alert1.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            present(alert1, animated: true, completion: nil)
+        }
     }
     
     func boldTextJosia()-> NSAttributedString
@@ -122,5 +140,16 @@ class MovementPlanViewController: UIViewController {
         if let send = prayerDefaults.string(forKey: "mySend") {
             sendtext.text = send
         }
+    }
+}
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
     }
 }
